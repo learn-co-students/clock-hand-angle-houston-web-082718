a4cf6e4d94ee8bd73da9e3707b08980c2f3f4a65
Naeblis <johnmlowery@gmail.com>
2018-09-11 10:20:29 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,18 @@
+
+require 'pry'
+
+
 def clock_angle(time)
-    # code your solution here
+    hours = time.split(":")[0].to_f
+    minutes = time.split(":")[1].to_f
+    
+    minutes_angle = minutes * 6 
+    hours_angle = (30 * hours) + (0.5*minutes)
+    
+    angle = (minutes_angle - hours_angle).abs
+    angle = 360 - angle if angle > 180
+
+    angle
 end
+
+binding.pry
