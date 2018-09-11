3dec11bf135168555608115585d7346a7d72bb9f
github username <github email address>
2018-09-11 09:43:47 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,18 @@
 def clock_angle(time)
     # code your solution here
+require "pry"
+
+def hours_and_minutes_from_string(string)
+  angle_diff_from_time(string.split(":"))
+end
+
+def angle_diff_from_time(time_array)
+  hours = time_array[0].to_i
+  minutes = time_array[1].to_i
+  minutes_angle = minutes * 6
+  hour_angle = hours.to_f * 30 + minutes.to_f / 2
+  [hour_angle, minutes_angle, (minutes_angle - hour_angle).abs]
+end
+
+binding.pry
 end
