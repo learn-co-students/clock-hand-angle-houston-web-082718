f5d547c60a59d32265bf6b4295fd93f9bbc2c195
gwatson86 <gwatson86@gmail.com>
2018-09-11 10:22:40 -0500

    behold the 9 line glory

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,12 +1,9 @@
 def clock_angle(time)
-    h = time.split(':')[0].to_f
-    m = time.split(':')[1].to_f
-    
+    h, m = time.split(':')[0].to_f, time.split(':')[1].to_f
+
     angle = (6*m - (30*h + 0.5*m)).abs
     
-    if angle > 180
-        angle = 360 - angle
-    end
+    angle = 360 - angle if angle > 180
 
     angle  
 end
